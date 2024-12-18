51ebc5c3907b611cc5f1e1a523b219458f8d62dc
mancuoj <mancuoj@gmail.com>
2024-12-18 23:49:35 +0800

    chore: update

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {import.meta.env.PROD && (
+          <script
+            defer
+            src="https://a.mancuoj.me/script.js"
+            data-website-id="411294ea-0e35-4747-aedc-299f363b0fa3"
+          ></script>
+        )}
         <Meta />
         <Links />
       </head>
