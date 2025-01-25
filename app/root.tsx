3e97d40e3fa1e6ab4c7fa029292eeb7df8e8626a
mancuoj <mancuoj@gmail.com>
2025-01-26 01:28:01 +0800

    chore: fonts

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="font-sans antialiased">
+      <body className="font-dm antialiased">
         {children}
         <ScrollRestoration />
         <Scripts />
