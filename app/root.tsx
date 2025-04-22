4cb87ca6ac9cc03e61e4c61f4d377eddf6aae7de
mancuoj <mancuoj@gmail.com>
2025-04-23 01:29:49 +0800

    chore: re init

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,18 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse } from 'react-router'
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
 
-import { TwScreenIndicator } from '@/components/tw-screen-indicator'
-import type { Route } from './+types/root'
-import '@/app.css'
+import type { Route } from "./+types/root";
+import "@/app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-]
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -17,43 +23,44 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="font-sans antialiased">
+      <body>
         {children}
-        <TwScreenIndicator />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
 
 export default function App() {
-  return <Outlet />
+  return <Outlet />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = 'Oops!'
-  let details = 'An unexpected error occurred.'
-  let stack: string | undefined
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? '404' : 'Error'
+    message = error.status === 404 ? "404" : "Error";
     details =
-      error.status === 404 ? 'The requested page could not be found.' : error.statusText || details
+      error.status === 404
+        ? "The requested page could not be found."
+        : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message
-    stack = error.stack
+    details = error.message;
+    stack = error.stack;
   }
 
   return (
-    <main className="flex flex-col items-center min-h-svh justify-center p-8 gap-5">
-      <h1 className="text-3xl font-semibold">{message}</h1>
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full mockup-code bg-base-300 text-base-content">
-          <code className="text-xs p-5 font-medium">{stack}</code>
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
         </pre>
       )}
     </main>
-  )
+  );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,16 @@
-import type { Route } from './+types/home'
+import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: 'React Router v7' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ]
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
 }
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1 className="btn btn-primary">Vite is All You Need</h1>
+      <h1 className="text-2xl font-bold">React Router</h1>
     </div>
-  )
+  );
 }
