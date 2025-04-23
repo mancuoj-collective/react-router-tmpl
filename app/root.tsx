f5f0fcbc2cc0a580bf16390cba7072fca7f9abef
mancuoj <mancuoj@gmail.com>
2025-04-24 03:49:22 +0800

    feat: iconify and dark mode toggle

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,6 @@
 import type { Route } from './+types/root'
-import {
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from 'react-router'
+import { ThemeProvider } from 'next-themes'
+import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
 import '@/app.css'
 
 export const links: Route.LinksFunction = () => [
@@ -15,7 +9,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -23,7 +17,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body className="font-sans antialiased relative">
-        {children}
+        <ThemeProvider attribute="class" storageKey="remix-tmpl-theme">
+          {children}
+        </ThemeProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,5 @@
+import { ThemeToggle } from '@/components/theme-toggle'
+import { TwScreenIndicator } from '@/components/tw-screen-indicator'
 import { Button } from '@/components/ui/button'
 
 export function meta() {
@@ -9,8 +11,16 @@ export function meta() {
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>React Router</Button>
+    <div className="relative font-sans antialiased">
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <div className="flex gap-2">
+          <ThemeToggle />
+          <Button variant="outline" size="icon">
+            <a className="iconify carbon--logo-github size-4.5" href="https://github.com/mancuoj-collective/remix-tmpl" />
+          </Button>
+        </div>
+        <TwScreenIndicator />
+      </div>
     </div>
   )
 }
