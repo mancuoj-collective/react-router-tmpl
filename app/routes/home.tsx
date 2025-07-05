5926f80dfe280c41071e095f6fe9248c6029179e
mancuoj <mancuoj@gmail.com>
2025-07-05 13:28:42 +0800

    chore: update deps

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 export function meta() {
   return [
     { title: 'React Router' },
-    { name: 'description', content: 'Welcome to React Router!' },
+    { name: 'description', content: 'React router starter template' },
   ]
 }
 
@@ -13,11 +13,13 @@ export default function Home() {
   return (
     <div className="relative font-sans antialiased">
       <div className="flex flex-col items-center justify-center min-h-svh">
-        <div className="flex gap-2">
-          <ThemeToggle />
-          <Button variant="outline" size="icon">
-            <a className="iconify carbon--logo-github size-4.5" href="https://github.com/mancuoj-collective/remix-tmpl" />
+        <div className="flex gap-2.5 items-center">
+          <Button variant="outline">
+            <a href="https://github.com/mancuoj-collective/remix-tmpl">
+              GitHub
+            </a>
           </Button>
+          <ThemeToggle />
         </div>
         <TwScreenIndicator />
       </div>
